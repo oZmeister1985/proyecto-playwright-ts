ca6cbae8cded3d7e0a1fee335ccbfcb4c60414a0
ozzy <oswaldo.jacobo.unosquare123456@gmail.com>
2026-07-20 11:51:38 -0600

    feat: integrate allure-playwright for enhanced reporting
    - Added allure-playwright as a dependency in package.json and package-lock.json.
    - Configured Playwright to use allure-playwright as the reporter in playwright.config.ts.
    - Generated allure report files including statistic, tree, variables, environments, globals, nav, pie chart, quality gate, search index, timeline, and tree filters.
    - Created allure results for multiple test runs with detailed step information and status.

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  reporter: "allure-playwright",
   
   use: {
     headless: false,
